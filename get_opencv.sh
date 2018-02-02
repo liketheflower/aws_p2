fba4bd78ff1c2c236779611a6e9f497d96a64c70
jimmy shen <jim.morris.shen@gmail.com>
2018-02-02 16:43:16 -0500

    Update get_opencv.sh

--- get_opencv.sh
+++ get_opencv.sh
@@ -10,7 +10,7 @@ cd opencv
 mkdir release
 cd release
 cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON \ -D BUILD_NEW_PYTHON_SUPPORT=ON \ -D WITH_V4L=ON \ -D INSTALL_C_EXAMPLES=ON \ -D INSTALL_PYTHON_EXAMPLES=ON \ -D BUILD_EXAMPLES=ON \ -D WITH_QT=ON \ -D WITH_OPENGL=ON \ -D WITH_CUDA=ON \ -D ENABLE_FAST_MATH=1 \ -D CUDA_FAST_MATH=1 \ -D WITH_CUBLAS=1 \ -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
-make -j8
+make -j10
 sudo make install
 sudo ldconfig
 sudo ln /dev/null /dev/raw1394
